82d15244e57715d455e71f67e92cba6a9db02ded
Mateusz Cabała <mateusz.cabala95@gmail.com>
2022-01-24 21:56:04 +0100

    Add basic entities

---
+++ Blog.Model/Entities/IEntityBase.cs
@@ -0,0 +1,7 @@
+﻿namespace Blog.Model.Entities
+{
+    public interface IEntityBase
+    {
+        public string Id { get; set; }
+    }
+}
---
+++ Blog.Model/Entities/User.cs
@@ -0,0 +1,13 @@
+﻿namespace Blog.Model.Entities
+{
+    public record User: IEntityBase
+    {
+        public string Id { get; set; }
+
+        public string Username { get; set; }
+
+        public string Email { get; set; }
+
+        public string Password { get; set; }
+    }
+}
